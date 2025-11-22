f1dd7ea84701d58a9296d689bf8495f1266776a2
Jegadeesh25 <107478215+Jegadeesh25@users.noreply.github.com>
2025-11-22 03:51:18 -0800

    Delete payment directory

--- payment/src/main/java/com/ewallet/product/payment/repository/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ewallet.product.payment.repository;
-
-import com.ewallet.product.payment.dto.Customer;
-import com.ewallet.product.payment.dto.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MerchantRepository extends JpaRepository<Merchant, Long> {
-}
-
--- payment/src/main/java/com/ewallet/product/payment/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ewallet.product.payment.repository;
-
-import com.ewallet.product.payment.dto.Customer;
-import com.ewallet.product.payment.dto.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
-
